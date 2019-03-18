16963193a38cc93604608cd5ba16f43c3a5612e8
Somon Soum <somon.soum9@gmail.com>
2019-03-19 05:54:16 +0700

    Mathjax with Rails 5

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,11 @@
+//= require rails-ujs
+//= require activestorage
+//= require popper
+//= require jquery3
+//= require bootstrap.bundle.min
+//= require jqBootstrapValidation
+//= require jquery.min
+//= require select2.min
+//= require contact_me
+//= require custom
+
